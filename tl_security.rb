6b79111b7a178cb9518eda3ed9edc8c0fe6d0cfc
Toshiaki Asai <qtoship@gmail.com>
2011-10-27 03:26:23 +0900

    ミュートされたユーザを正常にマスキングできていなかった/高速化

--- tl_security.rb
+++ tl_security.rb
@@ -2,41 +2,49 @@
 
 # ＼爽快セキュリティ！／
 # サクサクサクサクサクサクサクサク…
+
+require 'set'
+
 Plugin.create(:tl_security) do
 
-  muted_users = Hash.new        # user_id => mute limit Time
-  update_count = Hash.new{ |h, k| h[k] = 0 } # user_id => count
+  @muted_users = Set.new        # user_id
+  @update_count = Hash.new{ |h, k| h[k] = 0 } # user_id => count
 
   filter_show_filter do |messages|
-    if muted_users
-      [messages.select{ |m| not muted_users.has_key?(m.id) }]
-    else
-      [messages] end end
+    messages.each &method(:regist)
+    [messages.select{ |m| !@muted_users.include?(m.user.id) }]
+  end
 
-  onappear do |messages|
-    now = Time.new.freeze
-    messages.each { |message|
+  class << self
+    def regist(message)
+      return if @muted_users.include?(message.user.id) or message.user.idname == 'mikutter_bot' or message.user.is_me?
+      now = Time.new.freeze
       limit = message[:created] + UserConfig[:tl_security_update_section]
+      # limit = now + UserConfig[:tl_security_update_section]
       if limit > now
-        count = atomic{ update_count[message.user.id] += 1 }
-        notice "@#{message.user.idname} #{count} #{limit}"
+        count = atomic{ @update_count[message.user.id] += 1 }
         Reserver.new(limit){
-          count = atomic{ update_count[message.user.id] -= 1 }
-          notice "@#{message.user.idname} #{count}" }
+          count = atomic{ @update_count[message.user.id] -= 1 } }
         if count >= UserConfig[:tl_security_update_limit]
-          Plugin.call :tl_security_temporary_mute, message.user, limit end end } end
+          temporary_mute message.user, now + UserConfig[:tl_security_mute_seconds] end end end
 
-  on_tl_security_temporary_mute do |user, mute_limit|
-    Plugin.call(:update, nil, [Message.new(:message => "@#{user.idname} を #{mute_limit} までミュートします",
-                                           :system => true)])
-    notice "temporary mute @#{user.idname} to #{mute_limit}"
-    muted_users[user.id] = mute_limit
-    Reserver.new(mute_limit){
-      Plugin.call :tl_security_temporary_unmute, user } end
+    def temporary_mute(user, mute_limit)
+      notice "temporary mute @#{user.idname} to #{mute_limit}"
+      Plugin.call(:update, nil, [Message.new(:message => "@#{user.idname} を #{mute_limit} までミュートします",
+                                             :system => true)])
+      Plugin.call :tl_security_temporary_mute, user, mute_limit
+      @muted_users << user.id
+      Reserver.new(mute_limit){
+        temporary_unmute(user) } end
 
-  on_tl_security_temporary_unmute do |user|
-    notice "unmute @#{user.idname}"
-    muted_users.delete(user.id) end
+    def temporary_unmute(user)
+      notice "unmute @#{user.idname}"
+      Plugin.call(:update, nil, [Message.new(:message => "@#{user.idname} のミュートを解除します",
+                                             :system => true)])
+      Plugin.call :tl_security_temporary_unmute, user
+      @muted_users.delete(user.id) end
+
+  end
 
   # この秒数の間にtl_security_update_limit回ツイートがあれば一定時間ミュート
   UserConfig[:tl_security_update_section] ||= 1
